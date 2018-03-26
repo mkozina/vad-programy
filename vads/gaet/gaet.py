633b69d6e56b3e6557cae35cb64e01bb7669226e
mkozina <mkozina@sigma.ug.edu.pl>
2018-03-26 03:21:19 +0200

    Calculating noise level using three different tangent lines

--- vads/gaet/gaet.py
+++ vads/gaet/gaet.py
@@ -37,6 +37,9 @@ if len(sys.argv) == 2:
 	# frame number (starts from 1)
 	frame_no = 0
 
+	# noise level of each frame
+	noise_level = array('d')
+
 	# main loop over each frame
 	for i in range(0, len(signal[1]), 160):
 
@@ -58,111 +61,136 @@ if len(sys.argv) == 2:
 		C_x = B_x
 		C_y = A_y
 
-		# values of points B' and A' from Tanyer-Ozer
-		# Bprim_y = 1/5, 1/10 and 1/20
-		# Aprim_y = 4/5, 9/10 and 19/20
-		# these values are observed to be not very critical
+		Q_x = array('d')
+		Q_y = array('d')
 
-		Bprim_value = round((N-1)/5)
-		Aprim_value = round(4*((N-1)/5))
+		stop = "no"
 
-		Bprim_x = x[Bprim_value]
-		Bprim_y = y[Bprim_value]
-		Aprim_x = x[Aprim_value]
-		Aprim_y = y[Aprim_value]
+		for ii in range(1, 4):
 
-		Aline_x = array('d')
-		Aline_y = array('d')
-		Bline_x = array('d')
-		Bline_y = array('d')
-		Qline_x = array('d')
-		Qline_y = array('d')
+			# values of points B' and A' from Tanyer-Ozer
+			# Bprim_y = 1/5, 1/10 and 1/20
+			# Aprim_y = 4/5, 9/10 and 19/20
+			# these values are observed to be not very critical
 
-		aAline = aline(A_x, A_y, Aprim_x, Aprim_y)
-		bAline = bline(A_x, A_y, Aprim_x, Aprim_y)
-		aBline = aline(B_x, B_y, Bprim_x, Bprim_y)
-		bBline = bline(B_x, B_y, Bprim_x, Bprim_y)
+			if ii == 1:
+				Bprim_value = round((N-1)/5)
+				Aprim_value = round(4*((N-1)/5))
+			elif ii == 2:
+				Bprim_value = round((N-1)/10)
+				Aprim_value = round(9*((N-1)/10))
+			elif ii == 3:
+				Bprim_value = round((N-1)/20)
+				Aprim_value = round(19*((N-1)/20))
 
-		# solve: y - ax = b
-		a = np.array([[1,-aAline],[1,-aBline]])
-		b = np.array([bAline,bBline])
-		c = linalg.solve(a,b)
-		Qprim_x = c[1]
-		Qprim_y = c[0]
+			Bprim_x = x[Bprim_value]
+			Bprim_y = y[Bprim_value]
+			Aprim_x = x[Aprim_value]
+			Aprim_y = y[Aprim_value]
 
-		aQline = aline(C_x, C_y, Qprim_x, Qprim_y)
-		bQline = bline(C_x, C_y, Qprim_x, Qprim_y)
+			Aline_x = array('d')
+			Aline_y = array('d')
+			Bline_x = array('d')
+			Bline_y = array('d')
+			Qline_x = array('d')
+			Qline_y = array('d')
 
-		for j in range(B_x, A_x+1):
-			Aline_x.append( j )
-			Aline_y.append( aAline*j + bAline )
-			Bline_x.append( j )
-			Bline_y.append( aBline*j + bBline )
-			Qline_x.append( j )
-			Qline_y.append( aQline*j + bQline )
+			aAline = aline(A_x, A_y, Aprim_x, Aprim_y)
+			bAline = bline(A_x, A_y, Aprim_x, Aprim_y)
+			aBline = aline(B_x, B_y, Bprim_x, Bprim_y)
+			bBline = bline(B_x, B_y, Bprim_x, Bprim_y)
 
-# #########################
-		# point Q
+			# solve: y - ax = b
+			a = np.array([[1,-aAline],[1,-aBline]])
+			b = np.array([bAline,bBline])
+			c = linalg.solve(a,b)
+			Qprim_x = c[1]
+			Qprim_y = c[0]
 
-		seg_base = math.ceil(len(x)/2)
-		seg_first = 0
-		seg_second = seg_base
-		prev_seg_first = -1
-		prev_seg_second = -1
+			aQline = aline(C_x, C_y, Qprim_x, Qprim_y)
+			bQline = bline(C_x, C_y, Qprim_x, Qprim_y)
 
-		while (seg_first != prev_seg_first) or (seg_second != prev_seg_second):
+			for j in range(B_x, A_x+1):
+				Aline_x.append( j )
+				Aline_y.append( aAline*j + bAline )
+				Bline_x.append( j )
+				Bline_y.append( aBline*j + bBline )
+				Qline_x.append( j )
+				Qline_y.append( aQline*j + bQline )
 
-			firstQ = aQline*x[seg_first] + bQline
-			secondQ = aQline*x[seg_second] + bQline
+			plt.plot(Aline_x, Aline_y, 'r-')
+			plt.plot(Bline_x, Bline_y, 'g-')
+			plt.plot(Qprim_x, Qprim_y, 'ko')
+			plt.plot(Qline_x, Qline_y, 'k-')
 
-			prev_seg_first = seg_first
-			prev_seg_second =	seg_second
-			seg_base = math.ceil(seg_base/2)
+			seg_base = math.ceil(len(x)/2)
+			seg_first = 0
+			seg_second = seg_base
+			prev_seg_first = -1
+			prev_seg_second = -1
 
-			if y[seg_first] <= firstQ and y[seg_second] <= secondQ:
-				seg_first = seg_second
-				seg_second = seg_second + seg_base
-			elif y[seg_first] <= firstQ and y[seg_second] > secondQ:
-				seg_second = seg_first + seg_base
+			while (seg_first != prev_seg_first) or (seg_second != prev_seg_second):
 
-		x_regress = array('d')
-		y_regress = array('d')
+				firstQ = aQline*x[seg_first] + bQline
+				secondQ = aQline*x[seg_second] + bQline
 
-		for k in range(0, 6):
-			x_regress.append( x[seg_first-k] )
-			y_regress.append( y[seg_first-k] )
-			plt.plot(x[seg_first-k], y[seg_first-k], 'yo')
+				prev_seg_first = seg_first
+				prev_seg_second =	seg_second
+				seg_base = math.ceil(seg_base/2)
 
-		for k in range(0, 6):
-			x_regress.append( x[seg_second+k] )
-			y_regress.append( y[seg_second+k] )
-			plt.plot(x[seg_second+k], y[seg_second+k], 'yo')
+				if y[seg_first] <= firstQ and y[seg_second] <= secondQ:
+					seg_first = seg_second
+					seg_second = seg_second + seg_base
+				elif y[seg_first] <= firstQ and y[seg_second] > secondQ:
+					seg_second = seg_first + seg_base
 
-		(slope, intercept, r_value, p_value, std_err) = stats.linregress(x_regress, y_regress)
+				if seg_second > 400-5:
+					for t in range(1, 4-(ii-1)):
+						Q_x.append( x[len(x)-1] )
+						Q_y.append( y[len(y)-1] )
+					stop = "yes"
+					break
 
-		Rline_x = array('d')
-		Rline_y = array('d')
+			if "yes" in stop:
+				break
 
-		for l in range(0, 12):
-			Rline_x.append( x_regress[l] )
-			Rline_y.append( slope*x_regress[l] + intercept )
+			x_regress = array('d')
+			y_regress = array('d')
 
-		plt.plot(Rline_x, Rline_y, 'm')
+			for k in range(0, 6):
+				x_regress.append( x[seg_first-k] )
+				y_regress.append( y[seg_first-k] )
+				plt.plot(x[seg_first-k], y[seg_first-k], 'yo')
 
-		# solve: y - ax = b
-		a = np.array([[1,-aQline],[1,-slope]])
-		b = np.array([bQline,intercept])
-		c = linalg.solve(a,b)
-		Q_x = c[1]
-		Q_y = c[0]
+			for k in range(0, 6):
+				x_regress.append( x[seg_second+k] )
+				y_regress.append( y[seg_second+k] )
+				plt.plot(x[seg_second+k], y[seg_second+k], 'yo')
 
-		plt.plot(Q_x, Q_y, 'mo')
+			(slope, intercept, r_value, p_value, std_err) = stats.linregress(x_regress, y_regress)
 
-		#safety coefficient alpha (0.8 < alpha < 1.2)
+			Rline_x = array('d')
+			Rline_y = array('d')
+
+			for l in range(0, 12):
+				Rline_x.append( x_regress[l] )
+				Rline_y.append( slope*x_regress[l] + intercept )
+
+			plt.plot(Rline_x, Rline_y, 'm')
+
+			# solve: y - ax = b
+			a = np.array([[1,-aQline],[1,-slope]])
+			b = np.array([bQline,intercept])
+			c = linalg.solve(a,b)
+			Q_x.append( c[1] )
+			Q_y.append( c[0] )
+
+			plt.plot(Q_x[ii-1], Q_y[ii-1], 'mo')
+
+		#safety coefficient (0.8 < alpha < 1.2)
 		alpha = 1
-		noise_level = alpha*Q_x
 
-# #########################
+		noise_level.append( alpha*((Q_x[0]+Q_x[1]+Q_x[2])/3) )
 
 		# plot only first 149 frames (ca 1505ms)
 		if i <= 23680:
@@ -170,10 +198,6 @@ if len(sys.argv) == 2:
 
 			# plot GAET
 			plt.scatter(x, y)
-			plt.plot(Aline_x, Aline_y, 'r-')
-			plt.plot(Bline_x, Bline_y, 'g-')
-			plt.plot(Qprim_x, Qprim_y, 'ko')
-			plt.plot(Qline_x, Qline_y, 'k-')
 			plt.title("%s, frame: %i" % (filename, frame_no))
 			mng = plt.get_current_fig_manager()
 			mng.resize(*mng.window.maxsize())
@@ -181,6 +205,7 @@ if len(sys.argv) == 2:
 
 			# plot frame
 			plt.plot(signal_samples[i:i+N])
+			plt.axhline(y=noise_level[frame_no-1], color='m')
 			plt.ylabel("Amplitude")
 			plt.xlabel("Time (samples)")
 			plt.title("%s, frame: %i" % (filename, frame_no))
