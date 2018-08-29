75d408d74de3a8618bee66c5e1ac974c4dfe7578
mkozina <mkozina@sigma.ug.edu.pl>
2018-08-29 22:25:52 +0200

    Save voice-active decision to file

--- vads/et/et.py
+++ vads/et/et.py
@@ -23,6 +23,9 @@ if len(sys.argv) == 3:
 	signal_samples = signal[1]
 	signal_samples = signal_samples.astype(np.int64)
 
+	filename2 = filename+".detection.txt"
+	vad_file = open(filename2, 'w')
+
 	# frame size: 25ms (400 samples)
 	# overlapping 15ms (240 samples)
 	N = 400
@@ -50,6 +53,8 @@ if len(sys.argv) == 3:
 
 	energy_r = (1/frame_no)*energy
 
+	frame_no = 0
+
 	# main loop over each frame
 	for i in range(0, len(signal[1]), 160):
 
@@ -57,11 +62,13 @@ if len(sys.argv) == 3:
 		if len(x) < N:
 			break
 
+		frame_no += 1
+
 		# energy detector
 		# calculate energy of a frame
 		energy = sum(np.square( x )/N)
 
-		k = 1
+		k = 1.2
 		p = 0.2
 
 		if energy > k*energy_r:
@@ -69,6 +76,10 @@ if len(sys.argv) == 3:
 		else:
 			vad_decision.append( 0 )
 			energy_r = ((1-p)*energy_r) + (p*energy)
+
+		vad_file.write("%i \n" % vad_decision[frame_no-1])
+
+	vad_file.close()
 
 elif len(sys.argv) < 3:
 	print("You must enter noise and audio filenames as parameter!")
